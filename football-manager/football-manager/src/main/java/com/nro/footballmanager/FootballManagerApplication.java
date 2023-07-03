426324e737cf96aefad2e104eb521d730e79a1cd
SzAnita <97391261+SzAnita@users.noreply.github.com>
2023-07-03 18:40:05 +0300

    initialize project

--- football-manager/football-manager/src/main/java/com/nro/footballmanager/FootballManagerApplication.java
+++ football-manager/football-manager/src/main/java/com/nro/footballmanager/FootballManagerApplication.java
@@ -2,8 +2,9 @@ package com.nro.footballmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FootballManagerApplication {
 
 	public static void main(String[] args) {
